6dc81570915fecd793a1c64c8e94d57c93b33729
RishixD <82691180+RishixD@users.noreply.github.com>
2021-04-27 19:12:14 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,9 +8,9 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, 𝗜'𝗺 ✨ 𝗜𝗡𝗦𝗔𝗡𝗘 ™ ✨
+        f"""**Hey, 𝗜'𝗺 𖤍﷼༺𓆩ＡＮＵＳＨＫＡ𓆪•᭄♡﷼𖤍
 
-I can play music in your group's voice call. Developed by [𝗔NSHIKA🥰](https://t.me/Anshika_4).
+I can play music in your group's voice call. Developed by [⚡ＡＮＵＳＨＫＡ⚡](https://t.me/Secret_girl_Anushka).
 
 Add me to your group and play music freely!**
         """,
@@ -18,17 +18,17 @@ Add me to your group and play music freely!**
             [
                 [
                     InlineKeyboardButton(
-                        "🔥 𝚈𝙾𝚄𝚁 𝙼𝙰𝙺𝙴𝚁 🔥", url="https://t.me/GOD_INSANE")
+                        "🔥 𝚈𝙾𝚄𝚁 𝙼𝙰𝙺𝙴𝚁 🔥", url="https://t.me/Secret_girl_anushka")
                   ],[
                     InlineKeyboardButton(
-                        "🔰 𝙶𝚁𝙾𝚄𝙿 🔰", url="https://t.me/GODMODCHATS"
+                        "🔰 𝙶𝚁𝙾𝚄𝙿 🔰", url="https://t.me/secret_society_new"
                     ),
                     InlineKeyboardButton(
                         "🎛️ 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 🎛️", url="https://telegra.ph/MusicBot-Robot-MusicBot-Robo-03-14"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "😎 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 😎", url="https://t.me/INSANE_MUSIC_BOT?startgroup=true"
+                        "😎 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 😎", url="https://t.me/AnuMusicBot?startgroup=true"
                     )]
             ]
         ),
@@ -37,12 +37,12 @@ Add me to your group and play music freely!**
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**✨𝗜𝗡𝗦𝗔𝗡𝗘✨ is on fire 🔥 ✅**""",
+      await message.reply_text("""**𖤍﷼༺𓆩ＡＮＵＳＨＫＡ𓆪•᭄♡﷼𖤍 is on fire 🔥 ✅**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "😈 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙻 😈", url="https://t.me/@always_nd_foreverr")
+                        "😈 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 😈", url="https://t.me/secret_society_new")
                 ]
             ]
         )
