712937ac1104e3bb8ad9c1f447c6274ea7c8cc01
RishixD <82691180+RishixD@users.noreply.github.com>
2021-04-27 15:02:47 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,7 +8,7 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, 𝗜'𝗺 ✨ 𝗔𝗡𝗦𝗛𝗜𝗞𝗔'𝗦 𝗠𝗨𝗦𝗜𝗖 𝗣𝗟𝗔𝗬𝗘𝗥 ™ ✨
+        f"""**Hey, 𝗜'𝗺 ✨ 𝗜𝗡𝗦𝗔𝗡𝗘 ™ ✨
 
 I can play music in your group's voice call. Developed by [𝗔NSHIKA🥰](https://t.me/Anshika_4).
 
@@ -18,17 +18,17 @@ Add me to your group and play music freely!**
             [
                 [
                     InlineKeyboardButton(
-                        "🤞🏻𝚈𝙾𝚄𝚁 𝙼𝙰𝙺𝙴𝚁🤞🏻", url="https://t.me/Anshika_4")
+                        "🔥 𝚈𝙾𝚄𝚁 𝙼𝙰𝙺𝙴𝚁 🔥", url="https://t.me/GOD_INSANE")
                   ],[
                     InlineKeyboardButton(
-                        "🔰𝙶𝚁𝙾𝚄𝙿🔰", url="https://t.me/Schoolmastii"
+                        "🔰 𝙶𝚁𝙾𝚄𝙿 🔰", url="https://t.me/GODMODCHATS"
                     ),
                     InlineKeyboardButton(
                         "🎛️ 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 🎛️", url="https://telegra.ph/MusicBot-Robot-MusicBot-Robo-03-14"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "😎 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 😎", url="https://t.me/ANSHIKA_MUSIC_BOT?startgroup=true"
+                        "😎 𝙰𝙳𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 😎", url="https://t.me/INSANE_MUSIC_BOT?startgroup=true"
                     )]
             ]
         ),
