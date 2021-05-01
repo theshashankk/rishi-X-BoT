3f0b3aa6b5914d550f1a5588c1f90dfd5edf066f
RishixD <82691180+RishixD@users.noreply.github.com>
2021-05-01 14:16:33 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Downloading the song by @UnknownHacker001...")
+    m.edit("Downloading the song by @xD_Rishi...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @UnknownHacker001**'
+        rep = '**🎵 Uploaded by [⚡ 𝐑𝐢𝐒𝐇𝐢_𝐱𝐃 ⚡](https://t.me/xD_Rishi).**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
