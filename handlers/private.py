be67d07864e9343fd986bcca609316c355838fec
RishixD <82691180+RishixD@users.noreply.github.com>
2021-05-02 20:38:24 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,9 +8,9 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey there, 𝗜'𝗺 ⚡ 𝐂𝐎𝐅𝐅𝐈𝐍 𝐌𝐔𝐒𝐈𝐂 ™ ⚡
+        f"""**Hey there, 𝗜'𝗺 ⚡ 𝐌α𝐅ια 𝐂яα𝐙𝚈 𝐕𝐜 𝐁σт ™ ⚡
 
-I can play music in your group's voice call. Developed by [⚡ 𝐑𝐢𝐒𝐇𝐢_𝐱𝐃 ⚡](https://t.me/xD_Rishi).
+I can play music in your group's voice call. Developed by [⚡ 𝐌𝐀𝐅𝐈𝐀_𝐂𝐑𝐀𝐙𝐘 ⚡](https://t.me/MAFIAxCRAZY).
 
 Add me to your group and play music freely!**
         """,
@@ -18,17 +18,17 @@ Add me to your group and play music freely!**
             [
                 [
                     InlineKeyboardButton(
-                        "♻ 𝐎𝐖𝐍𝐄𝐑 ♻", url="https://t.me/xD_Rishi")
+                        "♻ 𝐎𝐖𝐍𝐄𝐑 ♻", url="https://t.me/MAFIAxCRAZY")
                   ],[
                     InlineKeyboardButton(
-                        "🔰 𝐆𝐑𝐎𝐔𝐏 🔰", url="https://t.me/NiceJokeLol"
+                        "🔰 𝐆𝐑𝐎𝐔𝐏 🔰", url="https://t.me/music_groupop")
                     ),
                     InlineKeyboardButton(
                         "🎛️ 𝐌𝐀𝐍𝐔𝐀𝐋 🎛️", url="https://telegra.ph/MusicBot-Robot-MusicBot-Robo-03-14"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "➕ 𝐀𝐃𝐃 𝐓𝐎 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 ➕", url="https://t.me/MusicExeBot?startgroup=true"
+                        "➕ 𝐀𝐃𝐃 𝐓𝐎 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 ➕", url="https://t.me/MAFIAxCRAZY_MUSICxBOT?startgroup=true"
                     )]
             ]
         ),
@@ -37,12 +37,12 @@ Add me to your group and play music freely!**
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**⚡ 𝐂𝐎𝐅𝐅𝐈𝐍 ™ ⚡ is on fire 🔥 ✅**""",
+      await message.reply_text("""**⚡ 𝐌α𝐅ια 𝐂яα𝐙𝚈 𝐕𝐜 𝐁σт ™ ⚡ is on fire 🔥 ✅**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "😈 𝐇𝐄𝐑𝐄 𝐈𝐒 𝐌𝐘 𝐇𝐄𝐋𝐋 😈", url="https://t.me/Definitely_not")
+                        "😈 𝐇𝐄𝐑𝐄 𝐈𝐒 𝐌𝐘 𝐇𝐄𝐋𝐋 😈", url="https://t.me/music_groupop")
                 ]
             ]
         )
